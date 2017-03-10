57b655759935a60b3722829d2d0d6a32c440bfc5
Alexey Park <alexey.g.park@gmail.com>
2017-03-10 17:41:32 +0600

    Add paperclip + add image for post + validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  validates :image, presence: true
+
+  has_attached_file :image,
+                    styles: { medium: "640x", thumb: '100x100>' },
+                    default_url: '/images/:style/missing.png'
+
+  validates_attachment_content_type :image,
+                                    :content_type => /\Aimage\/.*\Z/
+
 end
