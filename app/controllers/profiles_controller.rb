074965fbe4fa01bbd2b9c30bc99c0fd5104571ee
Alexey Park <alexey.g.park@gmail.com>
2017-03-13 01:01:39 +0600

    Add ajax for follow/unfollow, style changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -23,15 +23,13 @@ class ProfilesController < ApplicationController
 
   def following
     @title = "Following"
-    @user  = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
+    @users = @user.following.page
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
-    @user  = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.page
     render 'show_follow'
   end
 
