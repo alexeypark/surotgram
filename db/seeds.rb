a54ec16c04e1c4325732b047cc1b290b05eab1b8
Alexey Park <alexey.g.park@gmail.com>
2017-03-13 18:44:21 +0600

    Change seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([{ user_name: 'alexey', email: 'alexey@mail.com', password: 'password', avatar: File.open(File.join(Rails.root, 'avatar', 'avatar.jpg')) }])
+User.create([{ user_name: 'alexey', email: 'alexey@mail.com', password: 'password', avatar: File.open(File.join(Rails.root,'app', 'assets', 'images', 'avatar', 'avatar.jpg')) }])
 50.times do
   User.create([{ user_name: Faker::Internet.user_name, email: Faker::Internet.free_email, password: 'password'}])
 end
@@ -19,8 +19,8 @@ following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
 
 20.times do
-  Post.create([{caption: Faker::Lorem.sentence(4), image: File.open(Dir.glob(File.join(Rails.root, 'sampleimages', '*')).sample), user_id: following.sample.id }])
-  Post.create([{caption: Faker::Lorem.sentence(4), image: File.open(Dir.glob(File.join(Rails.root, 'sampleimages', '*')).sample), user_id: user.id }])
+  Post.create([{caption: Faker::Lorem.sentence(4), image: File.open(Dir.glob(File.join(Rails.root,'app', 'assets', 'images', 'sampleimages', '*')).sample), user_id: following.sample.id }])
+  Post.create([{caption: Faker::Lorem.sentence(4), image: File.open(Dir.glob(File.join(Rails.root,'app', 'assets', 'images', 'sampleimages', '*')).sample), user_id: user.id }])
 end
 
 following.each { |followed| Post.all.sample.liked_by followed }
